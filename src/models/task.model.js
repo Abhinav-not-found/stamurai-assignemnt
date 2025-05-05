d6717cb142cab48fd4d6bc5897667f915314c8ab
abhinav <abhinavisgreat18@gmail.com>
2025-05-05 15:51:25 +0530

    feat: Add Task model

---
+++ src/models/task.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title:{
+    type:String,
+    required:[true,'title is required'],
+  },
+  description:{
+    type:String,
+    required:[true,'description is required'],
+  },
+  date:{
+    type:Date,
+    required:[true,'date is required'],
+  },
+  priority:{
+    type:String,
+    required:[true,'priority is required'],
+    enum:['low','medium','high'],
+    default:'low'
+  },
+  status:{
+    type:String,
+    required:[true,'status is required'],
+    enum:['pending','inProgress','completed'],
+    default:'pending'
+  },
+  assignedUsers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: [true, 'assignedUsers is required'],
+    ref: 'User'
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }
+  
+},{
+  timestamps:true
+})
+
+const Task = mongoose.models.task || mongoose.model('task', taskSchema)
+
+export default Task
+

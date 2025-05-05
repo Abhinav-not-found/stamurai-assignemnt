3eed0e680d504c25ac07c999add119122cf69623
abhinav <abhinavisgreat18@gmail.com>
2025-05-05 15:42:27 +0530

    feat: updated createTaskComponent

--- src/components/CreateTaskComponent.jsx
+++ src/components/CreateTaskComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -89,6 +89,21 @@ const frameworks = [
 ];
 
 const CreateTaskComponent = () => {
+  const [title,setTitle]=useState('');
+  const [description,setDescription]=useState('');
+  const [date,setDate]=useState('');
+  const [priority,setPriority]=useState('low');
+  const [status,setStatus]=useState('pending');
+  const [assignedUsers,setAssignedUsers]=useState([]);
+
+  const handleSaveTask = () =>{
+    try {
+      
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -102,23 +117,25 @@ const CreateTaskComponent = () => {
         <form className='space-y-4 '>
           <div className='space-y-2'>
             <Label htmlFor='title'>Title</Label>
-            <Input id='title' placeholder='Enter task title' />
+            <Input value={title} onChange={(e)=>setTitle(e.target.value)} id='title' placeholder='Enter task title' />
           </div>
 
           <div className='space-y-2'>
             <Label htmlFor='description'>Description</Label>
-            <Textarea id='description' placeholder='Task details...' />
+            <Textarea value={description} onChange={(e)=>setDescription(e.target.value)} id='description' placeholder='Task details...' />
           </div>
 
           <div className='grid grid-cols-2 gap-4'>
             <div className='space-y-2'>
               <Label htmlFor='due-date'>Due Date</Label>
-              <Input id='due-date' type='date' />
+              <Input value={date} onChange={(e)=>setDate(e.target.value)} id='due-date' type='date' />
             </div>
+
+
 
             <div className='space-y-2'>
               <Label htmlFor='priority'>Priority</Label>
-              <Select defaultValue='low'>
+              <Select value={priority} onValueChange={setPriority} >
                 <SelectTrigger id='priority'>
                   <SelectValue placeholder='Select' />
                 </SelectTrigger>
@@ -130,10 +147,12 @@ const CreateTaskComponent = () => {
               </Select>
             </div>
           </div>
+
+
           <div className='grid grid-cols-2 gap-4'>
             <div className='space-y-2'>
               <Label htmlFor='status'>Status</Label>
-              <Select defaultValue='pending'>
+              <Select value={status} onValueChange={setStatus} >
                 <SelectTrigger id='status'>
                   <SelectValue placeholder='Select' />
                 </SelectTrigger>
@@ -152,7 +171,8 @@ const CreateTaskComponent = () => {
                 commandProps={{
                   label: "Assign Users",
                 }}
-                value={[]}
+                value={assignedUsers}
+                onChange={setAssignedUsers}
                 defaultOptions={frameworks}
                 placeholder='Assign Users'
                 hideClearAllButton
